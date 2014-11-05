d3ad6cb0bbb4cbcdb7f066176ce43d2554496c65
Ezra <ezra@Ezras-MacBook-Pro.local>
2014-11-05 18:59:46 -0500

    push segue to detail page; mocked up beer data

--- Bukowski/BeerObject.h
+++ Bukowski/BeerObject.h
@@ -12,7 +12,16 @@
 @interface BeerObject : NSObject
 
 @property (strong, nonatomic) NSString *beerName;
-@property (strong, nonatomic) UIImage *beerImage;
+@property (strong, nonatomic) UIImage *bottleImage;
+@property (strong, nonatomic) NSString *brewery;
+@property (strong, nonatomic) NSString *beerStyle;
+@property (strong, nonatomic) NSString *beerDescription;
+@property (strong, nonatomic) NSString *abv;
+@property (strong, nonatomic) NSString *price;
+@property (strong, nonatomic) NSString *size;
+@property (strong, nonatomic) UIImage *glassImage;
+@property (strong, nonatomic) NSString *nickname;
+
 
 +(NSArray *)listOfBeers;
 
--- Bukowski/View Controllers/BKSMyFavoritesCollectionViewController.m
+++ Bukowski/View Controllers/BKSMyFavoritesCollectionViewController.m
@@ -8,8 +8,11 @@
 
 #import "BKSMyFavoritesCollectionViewController.h"
 #import "BeerObject.h"
+#import "BKSBeerDetailViewController.h"
 
 @interface BKSMyFavoritesCollectionViewController ()
+
+@property (nonatomic, strong) BeerObject *beerSelected;
 
 @end
 
@@ -21,9 +24,25 @@
     // Do any additional setup after loading the view.
 }
 
--(void)loadBeers
+- (void)loadBeers
 {
     self.beers = [BeerObject listOfBeers];
 }
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    self.beerSelected = [self.beers objectAtIndex:indexPath.item];
+    [self performSegueWithIdentifier:@"beerDetailSegue" sender:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"beerDetailSegue"]) {
+        BKSBeerDetailViewController *detailVC = (BKSBeerDetailViewController *)segue.destinationViewController;
+        detailVC.beer = self.beerSelected;
+    }
+    
+}
+
 
 @end
