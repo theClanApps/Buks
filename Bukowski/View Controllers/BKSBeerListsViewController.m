82c6ae16905445e0fac7cc65651cbaa81a60e506
Nick Servidio <nservidio@mobiquityinc.com>
2014-12-03 11:28:52 -0500

    started multi collection

--- Bukowski/View Controllers/BKSBeerListsViewController.m
+++ Bukowski/View Controllers/BKSBeerListsViewController.m
@@ -8,9 +8,28 @@
 
 #import "BKSBeerListsViewController.h"
 #import "BKSBeerCollectionTableViewCell.h"
+#import "BKSAccountManager.h"
+#import "UserBeerObject.h"
+//#import "BeerObject.h"
+
+typedef NS_ENUM(NSInteger, BKSBeerCategorySection) {
+    BKSBeerCategorySectionAllBeers,
+    BKSBeerCategorySectionBeersUnderEight,
+    BKSBeerCategorySectionBeersUnderFivePercent,
+};
+
+NSString * const kBKSAllBeers = @"All Beers";
+NSString * const kBKSBeersUnderEight = @"Don't Break the Bank";
+NSString * const kBKSBeersUnderFivePercent = @"Staying Soberish";
+
+NSInteger const kBKSNumberOfSections = 3;
 
 @interface BKSBeerListsViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) NSArray *allBeers;
+@property (strong, nonatomic) NSArray *beersUnderEight;
+@property (strong, nonatomic) NSArray *beersUnderFivePercent;
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation BKSBeerListsViewController
@@ -21,11 +40,29 @@
 }
 
 - (void)setup {
-    [self registerNibs];
+    [self loadBeers];
 }
 
-- (void)registerNibs {
-    
+- (void)sortAllBeersIntoCategories:(NSArray *)allBeers {
+    for (UserBeerObject *userBeerObject in self.allBeers) {
+        [userBeerObject.beer fetchIfNeeded];
+    }
+    self.beersUnderEight = [allBeers filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"(beer.price < %@)", @"7"]];
+}
+
+- (void)loadBeers {
+    [[BKSAccountManager sharedAccountManager] loadBeersWithSuccess:^(NSArray *beers, NSError *error) {
+        if (!error) {
+            self.allBeers = beers;
+            
+            [self sortAllBeersIntoCategories:self.allBeers];
+            
+            [self.tableView reloadData];
+        } else {
+            UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Error loading beers" message:@"Beers were unable to load" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [errorAlert show];
+        }
+    }];
 }
 
 - (void)setupFlowLayoutForCell:(BKSBeerCollectionTableViewCell *)cell {
@@ -40,16 +77,24 @@
 #pragma mark - UITableView
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return kBKSNumberOfSections;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 2;
+    return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    switch (section) {
+        case BKSBeerCategorySectionAllBeers: return kBKSAllBeers; break;
+        case BKSBeerCategorySectionBeersUnderEight: return kBKSBeersUnderEight; break;
+        case BKSBeerCategorySectionBeersUnderFivePercent: return kBKSBeersUnderFivePercent; break;
+        default: return nil; break;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     BKSBeerCollectionTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-    
     [self setupFlowLayoutForCell:cell];
     
     return cell;
