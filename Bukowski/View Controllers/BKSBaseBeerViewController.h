5263ff7e14148a1407c2679bd666cb4d46978a72
Ezra <ezra@Ezras-MacBook-Pro.local>
2015-01-05 21:49:20 -0500

    separated beerListsVC onto its own VC. Built generic VC

---
+++ Bukowski/View Controllers/BKSBaseBeerViewController.h
@@ -0,0 +1,13 @@
+//
+//  BKSBaseBeerViewController.h
+//  Bukowski
+//
+//  Created by Ezra on 1/5/15.
+//  Copyright (c) 2015 The Clan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BKSBaseBeerViewController : UIViewController
+
+@end
